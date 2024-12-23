b4f41af9505abd87747590057704dedb9e5b6bb6
Gamze <bilgegamze227@outlook.com>
2024-12-23 19:58:17 +0300

    Add project files

---
+++ doWhile/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // While Döngüsü
+        int limit;
+        do
+        {
+            Console.WriteLine("Bir limit değeri giriniz (0 veya daha büyük bir değer giriniz):");
+            limit = int.Parse(Console.ReadLine());
+            if (limit < 0)
+            {
+                Console.WriteLine("Hata: Negatif bir değer girdiniz. Lütfen 0 veya daha büyük bir değer giriniz.");
+            }
+        } while (limit < 0);
+
+        int sayac = 0;
+        while (sayac <= limit)
+        {
+            Console.WriteLine(sayac + ". Ben bir patikalıyım.");
+            sayac++;
+        }
+
+        //// Do-While Döngüsü
+        //int limit2;
+        //do
+        //{
+        //    Console.WriteLine("Bir limit değeri giriniz (0 veya daha büyük bir değer giriniz):");
+        //    limit2 = int.Parse(Console.ReadLine());
+        //    if (limit2 < 0)
+        //    {
+        //        Console.WriteLine("Hata: Negatif bir değer girdiniz. Lütfen 0 veya daha büyük bir değer giriniz.");
+        //    }
+        //} while (limit2 < 0);
+
+        //int sayac2 = 0;
+        //do
+        //{
+        //    Console.WriteLine(sayac2 + ". Ben bir patikalıyım.");
+        //    sayac2++;
+        //} while (sayac2 <= limit2);
+    }
+}
